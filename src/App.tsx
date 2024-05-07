d7aa37b5a32c4c5736fda378285a16a02c456586
syab-syab <syabsyabsyabsyab@gmail.com>
2024-05-07 21:21:38 +0900

    TypeScriptだと面倒

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,133 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home'
+// import Home from './components/Home'
+import Dexie, { Table } from 'dexie';
+import { useLiveQuery } from 'dexie-react-hooks'
+import { useState } from 'react';
+
+export interface Todo {
+  id?: number;
+  task: string;
+  completed: boolean;
+}
+
+export class MySubClassedDexie extends Dexie {
+  // 'todos' is added by dexie when declaring the stores()
+  // We just tell the typing system this is the case
+  todos!: Table<Todo>;
+
+  constructor() {
+    // super()にデータベース名を渡す(多分)
+    super('todoApp');
+    // バージョンを指定する
+    this.version(1).stores({
+        // オブジェクトストア(todos)の設定をする
+        // ++を付けることでオートインクリメントしプライマリーキーにする
+      friends: '++id, task, completed'
+    });
+  }
+}
+
+const db = new MySubClassedDexie();
+
+const { todos } = db
+
 
 function App() {
+  const [taskInput, setTaskInput] = useState<string>("")
+
+  // データベース内のすべてのtodosのデータを取得して配列にする(？)
+  // useLiveQueryはindexedDBのデータが更新されたときに再レンダリングする
+  const allItems: Array<Todo> | any = useLiveQuery(() => todos.toArray(), [])
+
+  const addTask = async (e: React.FormEvent<HTMLFormElement>) => {
+    // デフォルトのリロードを防ぐ(？)
+    e.preventDefault()
+    // const taskField = document.querySelector('#taskInput')
+    console.log('=====>', taskInput)
+
+    // addでデータの追加
+    await todos.add({
+      // task: taskField['value'],
+      task: taskInput,
+      completed: false,
+    })
+
+    // taskField['value'] = ''
+    setTaskInput("")
+  }
+
+  // deleteにプライマリーキーを指定して削除できる
+  const deleteTask = async (id: number | undefined) => todos.delete(id)
+
+  const toggleStatus = async (
+    id: number | any,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    // updateは更新したいデータのプライマリーキーを第一引数に指定し
+    // 第二引数に変更するプロパティとその値を指定する
+    await todos.update( id, {completed: !!e.target.checked })
+  }
+
+  const taskInputHandleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTaskInput(e.target.value)
+  }
+
+  // whereでプロパティを指定してequqalsで値を指定すれば検索できるが
+  // equalsはboolean値に対応していないっぽい(null、undefined、Objectsも未対応)
+  // 加えて文字列の比較では大文字と小文字が区別される
+  // const completedItems = todos.where('completed').equals(true).toArray()
+  // filterで代用した方が楽かもしれない
+  // const completedItems = allItems?.filter((item) => item.completed === true)
+  // console.log(completedItems)
+
   return (
-    <div className="App">
-      <Home />
+    <div className="container">
+      <p>{taskInput}</p>
+      <h3 className="teal-text center-align">Todo App</h3>
+      <form className="add-item-form" onSubmit={(e) => addTask(e)}>
+        <input
+          type="text"
+          className="itemField"
+          placeholder="What do you want to do today?"
+          id="taskInput"
+          value={taskInput}
+          onChange={taskInputHandleChange}
+          required
+        />
+        <button type="submit" className="waves-effect btn teal right">
+          Add
+        </button>
+      </form>
+
+      <div className="card white darken-1">
+        <div className="card-content">
+          {allItems?.map((item: Todo) => (
+            <div className="row" key={item.id}>
+              <p className="col s10">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={item.completed}
+                    className="checkbox-blue"
+                    onChange={(e) => toggleStatus(item.id, e)}
+                  />
+                  {/* completedがtrueの場合のみ打ち消し線(strike-text)を付ける */}
+                  <span className={`black-tex ${item.completed && 'strike-text'}`}>{item.task}</span>
+                </label>
+              </p>
+              <i
+                onClick={() => deleteTask(item.id)}
+                className="col s2 material-icons delete-button"
+              >
+                delete
+              </i>
+            </div>            
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,57 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-  let dbName: string = 'sampleDB'
-
-  let openReq: any = indexedDB.open(dbName);
-  //　DB名を指定して接続。DBがなければ新規作成される。
-
-  openReq.onupgradeneeded = (event: any) => {
-    //onupgradeneededは、DBのバージョン更新(DBの新規作成も含む)時のみ実行
-    console.log('db upgrade')
-  }
-
-  openReq.onsuccess = (event: any) => {
-    console.log('db open success')
-    let db: any = event.target.result;
-    // 接続を解除する
-    db.close()
-  }
-
-  openReq.onerror = (event: any) => {
-    console.log('db open error');
-  }
-
-
-  // データベース削除
-
-  // var deleteReq = indexedDB.deleteDatabase(dbName);
-
-  // deleteReq.onsuccess = function(event){
-  //   console.log('db delete success');
-  //   存在しないDB名を指定してもこっちが実行される
-  // }
-
-  // deleteReq.onerror = function(){
-  //   console.log('db delete error');
-  // }
-
-
-  // オブジェクトストア(テーブル)作成
-  let storeName: string = 'sampleStore';
-
-  let currentDateTime: any = new Date()
-
-  let dbVersion: number = currentDateTime.getTime()
-
-  let openReqSecond: any = indexedDB.open(dbName, dbVersion)
-  // オブジェクトストアの作成・削除はDBの更新時しかできないので、バージョンを指定して更新
-
-  openReqSecond.onupgradeneeded = (event: any) => {
-    let db: any = event.target.result;
-    db.createObjectStore(storeName, {keyPath : 'id'})
-  }
-
+  
 
   return (
     <>
