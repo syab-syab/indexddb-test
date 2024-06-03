c39c338ab87886fcd65185f301ba6363c1c1778f
syab-syab <syabsyabsyabsyab@gmail.com>
2024-06-04 04:29:25 +0900

    微修正

--- src/components/Deadline.tsx
+++ src/components/Deadline.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
 import { db } from '../models/db'
+import milliSecEdit from '../functions/milliSecEdit'
 
 const { deadline } = db
 
 const Deadline = () => {
+  // もしかしたら必要の無いstateがあるかも
   // id は自動でインクリメント
   const [name, setName] = useState<string>("")
   // ミリ秒二つ↓も一応文字列で型付け
+  // 期限のミリ秒
   const [deadline, setDeadline] = useState<string>("")
-  const [startSec, setStartSec] = useState<string>("")
+  // スタート時のミリ秒(このstateは必要無いかも)
+  // const [startSec, setStartSec] = useState<string>("")
+  // 期限を達成したか否か
   const [achievement, setAchievement] = useState<boolean>(false)
+  // 達成・未達成に関わらずカウントを終えたかどうか
   const [finished, setFinished] = useState<boolean>(false)
 
   const nameHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,15 +31,19 @@ const Deadline = () => {
     // デフォルトのリロードを防ぐ(？)
     e.preventDefault()
     
-    const tmpStart: number = Date.now()
-    setStartSec(String(tmpStart))
-    console.log(tmpStart)
+    // スタート時のミリ秒のした三桁を000にする
+    const startMilli: number = milliSecEdit(Date.now())
+    // setStateが効かない
+    // setStartSec(String(tmpStart))
+    console.log(startMilli)
     console.log(e)
-    console.log("startSec, ", startSec)
-    console.log(name, Number(deadline)*6000, startSec, achievement, finished)
+    // console.log("startSec, ", startSec)
+    console.log(name, (Number(deadline)*60000 + startMilli), startMilli, achievement, finished)
     setName('')
     setDeadline('')
-    setStartSec('')
+    // setStartSec('')
+
+    alert("カウント開始")
   }
 
   return (
@@ -41,6 +51,8 @@ const Deadline = () => {
       <p>Deadline</p>
       <div>
         <form onSubmit={(e) => addDeadline(e)}>
+        {/* ↑は本番 */}
+        {/* <form onSubmit={addDeadline}> */}
           <label>何を我慢する？</label>
           <input type="text" value={name} onChange={nameHandleChange} required />
           {/* 期限は短いのから試していくこと */}
